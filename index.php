30e8d0b71f488adb06190fc5fe13f65ffc1537d7
LeonV <leonvv25@gmail.com>
2021-08-06 13:58:57 +0300

    commit_06.08.2021_2

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
+for($i = 0; $i<=10; $i++) {
+    echo $i."<br>";
+}
 
-echo "test";
+// $i = 1;
+// do {
+//     echo $i."<br>";
+//     $i++;
+// } while($i<=10);
 
+// while($i<=10) {
+//     echo $i."<br>";
+//     $i++;
+// }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
