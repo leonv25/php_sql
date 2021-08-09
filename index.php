e8923ab4ec1770fd73cec1184ca9e715b6b37957
LeonV <leonvv25@gmail.com>
2021-08-09 23:02:58 +0300

    commit_09.08.2021

--- index.php
+++ index.php
@@ -1,18 +1,56 @@
 <?php
-for($i = 0; $i<=10; $i++) {
-    echo $i."<br>";
+
+
+$arr = ["продукты", "бутылка с водой"];
+
+// unset($arr[0]);
+
+array_push($arr, "яблоко");
+array_unshift($arr, "апельсин");
+array_push($arr, "апельсин");
+$arr = array_unique($arr);
+// $arr = array_reverse($arr);
+// sort($arr);
+// shuffle($arr);
+
+$arr2 = array("кокос", "арбуз");
+$newarr = array_merge($arr, $arr2);
+
+
+
+foreach($newarr as $key => $value) {
+    echo "Ключ к масиву ".$key."- Значение масива ".$value.'<br>';
 }
 
-// $i = 1;
-// do {
-//     echo $i."<br>";
-//     $i++;
-// } while($i<=10);
+// $str = implode(", ", $newarr);
+// echo $str;
 
-// while($i<=10) {
-//     echo $i."<br>";
-//     $i++;
-// }
+$str = "апельсин, продукты, бутылка с водой, яблоко, кокос, арбуз";
+$strtoarr = explode(", ", $str);
+echo "<pre>";
+print_r($strtoarr);
+echo "</pre>";
+
+$camera["market1"]["row1"][0] ='продукты';
+$camera["market1"]["row1"][1] ='сумка';
+$camera["market2"]["row2"][0] ='апельсин';
+$camera["market2"]["row2"][1] ='фотоапарат';
+$camera["market2"]["row2"][2] ='телефон';
+
+echo "<pre>";
+print_r($camera);
+echo "</pre>";
+
+foreach($camera as $key => $value) {
+    foreach($value as $key2 => $value2) {
+        foreach($value2 as $key3 => $value3) {
+            echo $value3.'<br>';
+        }
+    }
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
