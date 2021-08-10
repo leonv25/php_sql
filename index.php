7e6e1fa57cb453366c33931f06cf6867c4c21ace
LeonV <leonvv25@gmail.com>
2021-08-10 22:05:37 +0300

    commit_10.08.2021

--- index.php
+++ index.php
@@ -1,55 +1,9 @@
 <?php
-
-
-$arr = ["продукты", "бутылка с водой"];
-
-// unset($arr[0]);
-
-array_push($arr, "яблоко");
-array_unshift($arr, "апельсин");
-array_push($arr, "апельсин");
-$arr = array_unique($arr);
-// $arr = array_reverse($arr);
-// sort($arr);
-// shuffle($arr);
-
-$arr2 = array("кокос", "арбуз");
-$newarr = array_merge($arr, $arr2);
-
-
-
-foreach($newarr as $key => $value) {
-    echo "Ключ к масиву ".$key."- Значение масива ".$value.'<br>';
+// print_r ($_POST);
+if(isset($_POST['name'])) {
+    $nameFilter = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+    echo "Мое имя ".$nameFilter;
 }
-
-// $str = implode(", ", $newarr);
-// echo $str;
-
-$str = "апельсин, продукты, бутылка с водой, яблоко, кокос, арбуз";
-$strtoarr = explode(", ", $str);
-echo "<pre>";
-print_r($strtoarr);
-echo "</pre>";
-
-$camera["market1"]["row1"][0] ='продукты';
-$camera["market1"]["row1"][1] ='сумка';
-$camera["market2"]["row2"][0] ='апельсин';
-$camera["market2"]["row2"][1] ='фотоапарат';
-$camera["market2"]["row2"][2] ='телефон';
-
-echo "<pre>";
-print_r($camera);
-echo "</pre>";
-
-foreach($camera as $key => $value) {
-    foreach($value as $key2 => $value2) {
-        foreach($value2 as $key3 => $value3) {
-            echo $value3.'<br>';
-        }
-    }
-}
-
-
 
 ?>
 
@@ -62,7 +16,13 @@ foreach($camera as $key => $value) {
     <title>Document</title>
 </head>
 <body>
-    
+
+    <form action="/" method="post">
+        <input type="text" name="name" placeholder="ваше имя">
+        <textarea name="comment"></textarea>
+        <input type="hidden" name="id_user" value="1313">
+        <input type="submit" value="отправить">
+    </form>
     
 </body>
 </html>
