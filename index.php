01ba7ee95f66b2f97b453eb50edc5b1d49d6fe43
LeonV <leonvv25@gmail.com>
2021-08-12 20:57:49 +0300

    commit_12.08.2021

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "kinomonster");
+$ms = new mysqli("localhost", "root", "", "kinomonster");
 if(mysqli_connect_errno()) {
     printf ("Соединение не установлено 25", mysqli_connect_error());
     exit();
 }
 
+// $ms->set_charset("utf8");
 
-$mysqli->set_charset("utf8");
+$query = $ms->query("SELECT * FROM movie");
 
-
-
-$query = $mysqli->query("SELECT * FROM movie");
 while ($row = mysqli_fetch_assoc($query)) {
     echo $row['name'];
 }
 
-$mysqli->close();
-
-
-
+$ms->close();
 
 ?>
